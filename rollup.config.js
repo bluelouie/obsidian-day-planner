7ee76527c58541da1fd3c2484ee03807aa10fcef
Jose Medina <jmedinaluis@yahoo.com>
2023-07-26 08:56:02 -0500

    Updated config to use .env for easier project onboarding, prettierrc too

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,33 @@
-import typescript from '@rollup/plugin-typescript';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import copy from 'rollup-plugin-copy';
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import copy from "rollup-plugin-copy";
 import svelte from "rollup-plugin-svelte";
-import autoPreprocess from 'svelte-preprocess';
-
-const TEST_VAULT = 'test-vault/.obsidian/plugins/obsidian-day-planner';
+import autoPreprocess from "svelte-preprocess";
+import process from "process";
 
 export default {
-  input: 'src/main.ts',
+  input: "src/main.ts",
   output: {
-    dir: 'dist/',
-    sourcemap: 'inline',
-    format: 'cjs',
-    exports: 'default'
+    dir: "dist/",
+    sourcemap: "inline",
+    format: "cjs",
+    exports: "default",
   },
-  external: ['obsidian'],
+  external: ["obsidian"],
   plugins: [
     typescript(),
-    nodeResolve({browser: true}),
+    nodeResolve({ browser: true }),
     commonjs(),
     svelte({
-       preprocess: autoPreprocess()
+      preprocess: autoPreprocess(),
     }),
     copy({
       targets: [
-        { src: 'dist/main.js', dest: TEST_VAULT },
-        { src: ['manifest.json', 'styles.css'], dest: TEST_VAULT }
-      ], flatten: true
-    })
-  ]
+        { src: "dist/main.js", dest: process.env.TEST_VAULT },
+        { src: ["manifest.json", "styles.css"], dest: process.env.TEST_VAULT },
+      ],
+      flatten: true,
+    }),
+  ],
 };
